d65415ab81a2e5abe4bed1531a8ebfb7cf37dda0
wanglei <wanglei@ddex.io>
2019-04-11 15:33:14 +0800

    remove unuseful launcher log

--- launcher/launcher.go
+++ launcher/launcher.go
@@ -54,7 +54,6 @@ func (l *Launcher) Run() {
 				Valid:   true,
 			}
 
-			utils.Info("launchLog Data: %s", launchLog.Data)
 			signedRawTransaction := l.SignService.Sign(launchLog)
 			transactionHash, err := l.BlockChain.SendRawTransaction(signedRawTransaction)
 
