0f93a2add1fa7ef7ddb411b25983588f7eaa55bf
kevin.zhang <kevin.zhang@ddex.io>
2019-04-28 13:35:02 +0800

    remove package config & connection

--- common/queue_test.go
+++ common/queue_test.go
@@ -21,28 +21,6 @@ func (s *redisQueueTest) TearDownTest() {
 func (s *redisQueueTest) TearDownSuite() {
 }
 
-//func (s *redisQueueTest) TestPushAndPot() {
-//	redis := connection.NewRedisClient("redis://localhost:6379")
-//
-//	queue, err := InitQueue(&RedisQueueConfig{
-//		Name:   "test",
-//		Ctx:    context.Background(),
-//		Client: redis,
-//	})
-//
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	bts := make([]byte, 65535)
-//	rand.Read(bts)
-//
-//	_ = queue.Push(bts)
-//	bts2, _ := queue.Pop()
-//
-//	s.Equal(bts, bts2)
-//}
-
 func TestRedisQueue(t *testing.T) {
 	suite.Run(t, new(redisQueueTest))
 }
